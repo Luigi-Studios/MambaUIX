f272b8f3a3e374de27be4f358cc8433500f9a0dc
Luigi-Studios <155376785+Luigi-Studios@users.noreply.github.com>
2024-03-24 15:43:23 +0000

    added the desc for core function of the View class, multiple widgets.

--- SwiftUI.py
+++ SwiftUI.py
@@ -16,6 +16,17 @@ class View:
     def __init__(self):
         self.window = tkinter.Tk()
 
+    # shows the window on the screen by calling window.mainloop()
+
+    """ Why we need to do this:
+        1. We don't know, when the user stops adding elements to his widgets and by calling this method at the end,
+           we make sure that everything is ready
+        2. The way that the widgets are build by tkinter, we need to create all widgets first and then execute them
+           otherwise they won't work as expected"""
+
+    def start(self):
+        self.window.mainloop()
+
 
 # class Button to achieve Button() in main.py file
 
